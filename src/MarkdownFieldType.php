a1d4c1a852fba4b82672cce032523760edc2c486
Ryan Thompson <service@aiwebsystems.com>
2015-03-29 14:10:43 -0500

    Local storage

--- src/MarkdownFieldType.php
+++ src/MarkdownFieldType.php
@@ -1,6 +1,8 @@
 <?php namespace Anomaly\MarkdownFieldType;
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldType;
+use Anomaly\Streams\Platform\Application\Application;
+use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
 
 /**
  * Class MarkdownFieldType
@@ -27,4 +29,55 @@ class MarkdownFieldType extends FieldType
      */
     protected $inputView = 'anomaly.field_type.markdown::input';
 
+    /**
+     * The application utility.
+     *
+     * @var Application
+     */
+    protected $application;
+
+    /**
+     * Create a new EditorFieldType instance.
+     *
+     * @param Application $application
+     */
+    public function __construct(Application $application)
+    {
+        $this->application = $application;
+    }
+
+    /**
+     * Get the storage path.
+     *
+     * @return null|string
+     */
+    public function getStoragePath()
+    {
+
+        // If it's manually set just return it.
+        if ($path = $this->configGet('path')) {
+            return $path;
+        }
+
+        // No entry, no path.
+        if (!$this->entry) {
+            return null;
+        }
+
+        // If the entry is not an EntryInterface skip it.
+        if (!$this->entry instanceof EntryInterface) {
+            return null;
+        }
+
+        if (!$this->entry->getId()) {
+            return null;
+        }
+
+        $slug      = $this->entry->getStreamSlug();
+        $namespace = $this->entry->getStreamNamespace();
+        $folder    = str_slug($this->entry->getTitle(), '_');
+        $file      = $this->getField() . '.md';
+
+        return $this->application->getStoragePath("{$namespace}/{$slug}/{$folder}/{$file}");
+    }
 }
