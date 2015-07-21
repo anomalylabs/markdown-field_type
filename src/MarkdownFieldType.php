5ad035fe2aca239b96efe57b575b4d3fc99fa224
Ryan Thompson <service@aiwebsystems.com>
2015-07-21 11:17:14 -0500

    Protect against alternate entries

--- src/MarkdownFieldType.php
+++ src/MarkdownFieldType.php
@@ -62,7 +62,8 @@ class MarkdownFieldType extends FieldType
      */
     public function getFilePath()
     {
-        if (!$this->entry->exists) {
+        // If the entry is not an object or doesn't exist, skip it.
+        if (!is_object($this->entry) || !$this->entry || !$this->entry->exists) {
             return null;
         }
 
