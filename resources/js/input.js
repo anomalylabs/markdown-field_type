dda0a2a18c41efb94da7e05e88f893d82bc97466
Ryan Thompson <ryan@pyrocms.com>
2016-02-23 00:16:39 -0600

    Fix issue where input.js was looking for input rather than textarea.

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,7 +1,7 @@
 $(function () {
 
     // Initialize markdown inputs.
-    $('input[data-provides="anomaly.field_type.markdown"]').each(function () {
+    $('[data-provides="anomaly.field_type.markdown"]').each(function () {
         $(this).markdown({
             height: $(this).data('height'),
             iconlibrary: 'fa'
