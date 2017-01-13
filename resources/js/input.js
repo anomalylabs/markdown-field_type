c405c5624f9c4fb5b1cb8d1104335d76cbb61028
Ryan Thompson <ryan@pyrocms.com>
2017-01-13 02:24:24 -0600

    Init once

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,10 +1,13 @@
-$(function () {
+$(document).on('ajaxComplete ready', function () {
 
     // Initialize markdown inputs.
-    $('[data-provides="anomaly.field_type.markdown"]').each(function () {
-        $(this).markdown({
-            height: $(this).data('height'),
-            iconlibrary: 'fa'
-        });
+    $('[data-provides="anomaly.field_type.markdown"]:not([data-initialized])').each(function () {
+
+        $(this)
+            .attr('data-initialized', '')
+            .markdown({
+                height: $(this).data('height'),
+                iconlibrary: 'fa'
+            });
     });
 });
