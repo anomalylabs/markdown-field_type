a6a5c327f3b439c650be22e3135d7ef595d2cc08
Ryan Thompson <ryan@pyrocms.com>
2018-02-19 09:34:54 -0600

    Huh

--- resources/js/input.js
+++ resources/js/input.js
@@ -4,7 +4,24 @@
         document.querySelectorAll('textarea[data-provides="anomaly.field_type.markdown"]')
     );
 
+    let tabs = Array.from(
+        document.querySelectorAll('a[data-toggle="tab"]')
+    );
+
     fields.forEach(function (field) {
-        new SimpleMDE({element: field});
+
+        let markdown = new SimpleMDE({element: field});
+
+        if (tabs.length) {
+            tabs.forEach(function (tab) {
+                tab.addEventListener('shown.bs.tab', function () {
+                    alert();
+                    setTimeout(function () {
+                        markdown.codemirror.refresh();
+                    }, 10);
+                });
+            })
+        }
     });
+
 })(window, document);
