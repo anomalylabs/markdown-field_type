17da551faa364b19431c25b51853bec8ee62ee2c
Ryan Thompson <ryan@pyrocms.com>
2018-05-26 10:19:52 -0500

    Fallback put just in case

---
+++ src/MarkdownFieldTypeAccessor.php
@@ -0,0 +1,41 @@
+<?php namespace Anomaly\MarkdownFieldType;
+
+use Anomaly\MarkdownFieldType\Command\PutFile;
+use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAccessor;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+
+/**
+ * Class MarkdownFieldTypeAccessor
+ *
+ * @link   http://pyrocms.com/
+ * @author PyroCMS, Inc. <support@pyrocms.com>
+ * @author Ryan Thompson <ryan@pyrocms.com>
+ */
+class MarkdownFieldTypeAccessor extends FieldTypeAccessor
+{
+
+    use DispatchesJobs;
+
+    /**
+     * The field type instance.
+     * This is for IDE hinting.
+     *
+     * @var MarkdownFieldType
+     */
+    protected $fieldType;
+
+    /**
+     * Get the value off the entry.
+     *
+     * @return string
+     */
+    public function get()
+    {
+        if (!file_exists($this->fieldType->getStoragePath())) {
+            $this->dispatch(new PutFile($this->fieldType));
+        }
+
+        return parent::get();
+    }
+
+}
